8d5f15527bf1755c246b1a8bfc0a3a047d4ba891
alumno <alumno@30010577F138B8>
2023-03-07 13:37:26 +0100

    Añadiendo etiquetas HTML

---
+++ javadocpruebas/src/main/java/com/mycompany/mavenproject1/Prueba.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.mavenproject1;
+/**
+ * <h2> Esto es un ejemplo de documentacion </h2> 
+ * Puedo añadir etiquetas HTML, para <b>mejorar</b> la presentación.
+ * Por ejemplo añado unas viñetas
+ * <ul>
+ *  <li> Inserción de registros</li>
+ *  <li> Borrado y modificación de registros</li>
+ * </ul>
+ * @author SergioQuijada
+ * @version v1.0
+ */
+
+
+public class Prueba 
+{
+    
+}
